7cc83709633d65c1171a5152d2c6faa19008dd49
plong rotha <Mrr.rothabetta31@gmail.com>
2024-07-30 10:02:51 +0700

    update project water fee

--- Water fee/src/Wexcecute.java
+++ Water fee/src/Wexcecute.java
@@ -1,16 +1,23 @@
 import java.util.*;
+
 public class Wexcecute {
 
 	public static void main(String[] args) {
-		try(Scanner scan = new Scanner(System.in)){
+		try (Scanner scan = new Scanner(System.in)) {
 			int km2;
-			float price=450, total;
-			
-			System.out.print("Enter m2 ="); km2 = scan.nextInt();
+			float price = 450, total;
+
+			System.out.print("Enter m2 =");
+			km2 = scan.nextInt();
 			total = km2 * price;
-			System.out.println("Price that we need pay is ="+total+" riel ");			
+			System.out.println("Price that we need pay is =" + total + " riel ");
+			System.out.println("Price that we need pay is =" + total + " riel ");
+			System.out.println("Price that we need pay is =" + total + " riel ");
+			System.out.println("Price that we need pay is =" + total + " riel ");
+			System.out.println("Price that we need pay is =" + total + " riel ");
+			System.out.println("Price that we need pay is =" + total + " riel ");
 		}
- 
+
 	}
 
 }
